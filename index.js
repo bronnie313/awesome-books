cba2d0b040e9454bcdb2f85b6a0808df4e4d4903
sawmon <sawmonhan@acedatasystems.com>
2023-02-22 11:41:21 +0100

    fixed bug

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 const array = [];
 const btn = document.getElementById('addBtn');
-const section = document.getElementById('book-list')
-const close =document.getElementById('close');
+const section = document.getElementById('book-list');
+const close = document.getElementById('close');
 
-
-const addBook = (event) =>{
-    event.preventDefault();
-    let book = {
-        title: document.getElementById('title').value,
-        Author: document.getElementById('author').value
-    };
-    array.push(book);
-    localStorage.setItem('Booklist', JSON.stringify(array));
-    document.forms[0].reset();
-    section.innerHTML = '';
-    for(let i=0; i<=array.length; i+=1){
-        book = document.createElement('div');
-        book.className = 'bookList';
-        book.innerHTML = `
-        <h3>${array[i].title}</h3>
-        <h3>${array[i].Author}</h3>
-        <button type="button" onclick="removeBook(${array[i].id})">Remove</button>
-        <hr>
-        `;
-        section.appendChild(book);
-    }
+const addBook = (event) => {
+  event.preventDefault();
+  let book = {
+    Title: document.getElementById('title').value,
+    Author: document.getElementById('author').value,
+  };
+  array.push(book);
+  localStorage.setItem('Booklist', JSON.stringify(array));
+  document.forms[0].reset();
+  displayData();
 };
 
 btn.addEventListener('click', addBook);
 
 function displayData() {
-    let data = localStorage.getItem('Booklistt');
-    console.log(data);
+  let data = localStorage.getItem('Booklist');
+  let books = JSON.parse(data);
+  section.innerHTML = '';
+  if (books) {
+    books.forEach((book) => {
+      bookList = document.createElement('div');
+      bookList.className = 'bookList';
+      bookList.innerHTML = `
+        <h3>${book.Author}</h3>
+        <h3>${book.Title}</h3>
+        <button type="button" onclick="removeBook(${book.Title})">Remove</button>
+        <hr>
+        `;
+      section.appendChild(bookList);
+    });
+  }
 }
+
+displayData();
