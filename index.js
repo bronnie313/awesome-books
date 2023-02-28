f6c35914e128096069febeb612c42728f97b0eee
Bisaso Ronald <bronnie313@gmail.com>
2023-02-28 19:34:40 +0300

    Add changes to the file

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ class BookManager {
    
     this.books.forEach((book, index) => {
       const li = document.createElement('li');
-      li.innerHTML = `<div class="bookshow">${book.title} by ${book.author} <button onclick="bookManager.removeBook(${index})">Delete</button></div>`;
+      li.innerHTML = `<div class="bookshow">${book.title} by ${book.author} <button class="btn btn-outline-dark" onclick="bookManager.removeBook(${index})">Delete</button></div>`;
       booksList.appendChild(li);
     });
   }
@@ -59,3 +59,38 @@ bookForm.addEventListener('submit', (event) => {
 });
 
 bookManager.displayBooks();
+
+const linkNav = document.getElementById('list-nav');
+const addBookListNav = document.getElementById('add-book-list-nav');
+const contactNav = document.getElementById('contact-nav');
+
+linkNav.addEventListener('click', (e) => {
+  e.preventDefault();
+  hideSection();
+  link.style.display = 'flex';
+});
+
+addBookListNav.addEventListener('click', (e) => {
+  e.preventDefault();
+  hideSection();
+  addBookList.style.display = 'flex';
+});
+
+contactNav.addEventListener('click', (e) => {
+  e.preventDefault();
+  hideSection();
+  contact.style.display = 'flex';
+  contact.style.display = 'flex-direction = column';
+});
+
+const link = document.getElementById('list');
+const addBookList = document.getElementById('add-book-list');
+const contact = document.getElementById('contact');
+
+const hideSection = () => {
+  link.style.display = 'none';
+  addBookList.style.display = 'none';
+  contact.style.display = 'none';
+}
+
+
